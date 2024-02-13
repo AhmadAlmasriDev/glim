61ad3319de9bd6f0ed45d2e38802236dd9782144
Ahmad Almasri <ahmad.mohd.almasri@gmail.com>
2024-02-13 16:12:07 +0000

    Add responsiveness to the signin signup pages

--- src/components/sign/SignIn.jsx
+++ src/components/sign/SignIn.jsx
@@ -1,115 +1,125 @@
-import React, { useContext, useState}  from 'react'
-import { Form, Button,Alert} from "react-bootstrap";
-import { Link,useHistory } from "react-router-dom";
-import styles from './styles/SignInUp.module.css'
-import Logo from '../Logo'
-import backImage from '../../assets/signBackGround.jpg'
-import  DataContext  from '../../context/DataContext';
-import axios from 'axios';
-
-
+import React, { useContext, useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import styles from "./styles/SignInUp.module.css";
+import Logo from "../Logo";
+import backImage from "../../assets/signBackGround.jpg";
+import DataContext from "../../context/DataContext";
+import axios from "axios";
 
 const SignIn = () => {
-  const {setCurrentUser} = useContext(DataContext)
-  const [signInData, setSignInData] = useState({
-    username: '',
-    password: '',
-  })
+    const { setCurrentUser } = useContext(DataContext);
+    const [signInData, setSignInData] = useState({
+        username: "",
+        password: "",
+    });
 
-  const [errors, setErrors] = useState({})
+    const [errors, setErrors] = useState({});
 
-  const history = useHistory()
+    const history = useHistory();
 
-  const {username, password} = signInData
+    const { username, password } = signInData;
 
-  const handleChange = (event)=>{
-    setSignInData({
-      ...signInData,
-      [event.target.name]: event.target.value,
-    })
-  }
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value,
+        });
+    };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try{
-      const {data} =  await axios.post("/dj-rest-auth/login/", signInData)
-      setCurrentUser(data.user)
-      console.log(signInData)
-      history.push("/");
-    }
-    catch (err){
-      setErrors(err.response?.data);
-      console.log(signInData)
-      
-    }
-  }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await axios.post(
+                "/dj-rest-auth/login/",
+                signInData
+            );
+            setCurrentUser(data.user);
+            console.log(signInData);
+            history.push("/");
+        } catch (err) {
+            setErrors(err.response?.data);
+            console.log(signInData);
+        }
+    };
 
-  return (
-    <article className={`${styles.main_container} flex-container`}>
-      <section className={`${styles.form_container} v-flex-container`}>
-        <div>
-          <Logo className={styles.logo} height={130}/>
-        </div>
-        <h1 className={`${styles.form_header}`}>Sign in</h1>
-        <Form 
-        className={`${styles.form} v-flex-container`}
-        onSubmit={handleSubmit}
-        >
-          <Form.Group className={`${styles.form_input_container}`} controlId="username">
-            <Form.Label className={`display-non`}>User name</Form.Label>
-            <Form.Control 
-              type="text" 
-              placeholder="User name"
-              name = "username"
-              value = {username}
-              onChange={handleChange}
-              />
-            
-          </Form.Group>
-          {errors.username?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-          <Form.Group className={`${styles.form_input_container}`} controlId="password">
-            <Form.Label className={`display-non`}>Password</Form.Label>
-            <Form.Control 
-            type="password" 
-            placeholder="Password" 
-            name = "password"
-            value = {password}
-            onChange={handleChange}
+    return (
+        <article className={`${styles.main_container} flex-container`}>
+            <section className={`${styles.form_container} v-flex-container`}>
+                <div>
+                    <Logo className={styles.logo} height={80} />
+                </div>
+                <h1 className={`${styles.form_header}`}>Sign in</h1>
+                <Form
+                    className={`${styles.form} v-flex-container`}
+                    onSubmit={handleSubmit}
+                >
+                    <Form.Group
+                        className={`${styles.form_input_container}`}
+                        controlId="username"
+                    >
+                        <Form.Label className={`display-non`}>
+                            User name
+                        </Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="User name"
+                            name="username"
+                            value={username}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    {errors.username?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                    ))}
+                    <Form.Group
+                        className={`${styles.form_input_container}`}
+                        controlId="password"
+                    >
+                        <Form.Label className={`display-non`}>
+                            Password
+                        </Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            value={password}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Button
+                        className={`${styles.form_button} button`}
+                        type="submit"
+                    >
+                        Sign in
+                    </Button>
+                    <div className={`${styles.link_container} flex-container`}>
+                        <Link className={`${styles.signup_link}`} to="/signup">
+                            I don't have an account!
+                        </Link>
+                    </div>
+                    {errors.non_field_errors?.map((message, idx) => (
+                        <Alert key={idx} variant="warning" className="mt-3">
+                            {message}
+                        </Alert>
+                    ))}
+                </Form>
+            </section>
+            <section
+                className={`${styles.back_ground_container} flex-container`}
+            >
+                <div className={`${styles.image_container} flex-container`}>
+                    <img
+                        className={`${styles.back_ground_image} flex-container`}
+                        src={backImage}
+                        alt="Red cinema seats"
+                    />
+                </div>
+            </section>
+        </article>
+    );
+};
 
-            />
-          </Form.Group>
-          <Button className={`${styles.form_button} button`} type="submit">
-            Sign in
-          </Button>
-          <div className={`${styles.link_container} flex-container`}>
-            <Link className={`${styles.signup_link}`} to='/signup'>I don't have an account!</Link>
-          </div>
-          {errors.non_field_errors?.map((message, idx) => (
-              <Alert key={idx} variant="warning" className="mt-3">
-                {message}
-              </Alert>
-            ))}
-        </Form>
-        
-
-
-      </section>
-      <section className={`${styles.back_ground_container} flex-container`}>
-        <div className={`${styles.image_container} flex-container`}>
-          <img className={`${styles.back_ground_image} flex-container`} src={backImage} alt="Red cinema seats" />
-
-        </div>
-        
-      </section>
-
-
-    </article>
-  )
-    
-}
-
-export default SignIn
+export default SignIn;
--- src/components/sign/SignUp.jsx
+++ src/components/sign/SignUp.jsx
@@ -1,129 +1,143 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
-import styles from './styles/SignInUp.module.css'
-import Logo from '../Logo'
-import backImage from '../../assets/signBackGround.jpg'
-import axios from 'axios';
-
+import styles from "./styles/SignInUp.module.css";
+import Logo from "../Logo";
+import backImage from "../../assets/signBackGround.jpg";
+import axios from "axios";
 
 const SignUp = () => {
-  const [signUpData, setSignUpData] = useState({
-    username:'',
-    password1: '',
-    password2: '',
-  })
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password1: "",
+        password2: "",
+    });
 
-  const [errors, setErrors] = useState({})
+    const [errors, setErrors] = useState({});
 
-  const history = useHistory()
+    const history = useHistory();
 
-  const {username, password1,password2} = signUpData
+    const { username, password1, password2 } = signUpData;
 
-  const handleChange = (event)=>{
-    setSignUpData({
-      ...signUpData,
-      [event.target.name]: event.target.value,
-    })
-  }
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value,
+        });
+    };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try{
-      await axios.post("/dj-rest-auth/registration/", signUpData)
-      history.push("/signin");
-    }
-    catch (err){
-      setErrors(err.response?.data);
-    }
-  }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/registration/", signUpData);
+            history.push("/signin");
+        } catch (err) {
+            setErrors(err.response?.data);
+        }
+    };
 
-  return (
-    <article className={`${styles.main_container} flex-container`}>
-      <section className={`${styles.form_container} v-flex-container`}>
-        <div>
-          <Logo className={styles.logo} height={130}/>
-        </div>
-        <h1 className={`${styles.form_header}`}>Sign up</h1>
-        <Form 
-        className={`${styles.form} v-flex-container`}
-        onSubmit={handleSubmit}
-        >
-          <Form.Group className={`${styles.form_input_container}`} controlId="username">
-            <Form.Label className={`display-non`}>User name</Form.Label>
-            <Form.Control 
-              type="text" 
-              placeholder="User name"
-              name = "username"
-              value = {username}
-              onChange={handleChange}
-              />
-            
-          </Form.Group>
-          {errors.username?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-          <Form.Group className={`${styles.form_input_container}`} controlId="password1">
-            <Form.Label className={`display-non`}>Password</Form.Label>
-            <Form.Control 
-            type="password" 
-            placeholder="Password" 
-            name = "password1"
-            value = {password1}
-            onChange={handleChange}
+    return (
+        <article className={`${styles.main_container} flex-container`}>
+            <section className={`${styles.form_container} v-flex-container`}>
+                <div>
+                    <Logo className={styles.logo} height={80} />
+                </div>
+                <h1 className={`${styles.form_header}`}>Sign up</h1>
+                <Form
+                    className={`${styles.form} v-flex-container`}
+                    onSubmit={handleSubmit}
+                >
+                    <Form.Group
+                        className={`${styles.form_input_container}`}
+                        controlId="username"
+                    >
+                        <Form.Label className={`display-non`}>
+                            User name
+                        </Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="User name"
+                            name="username"
+                            value={username}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    {errors.username?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                    ))}
+                    <Form.Group
+                        className={`${styles.form_input_container}`}
+                        controlId="password1"
+                    >
+                        <Form.Label className={`display-non`}>
+                            Password
+                        </Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            name="password1"
+                            value={password1}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    {errors.password1?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                    ))}
+                    <Form.Group
+                        className={`${styles.form_input_container}`}
+                        controlId="password2"
+                    >
+                        <Form.Label className={`display-non`}>
+                            Confirm password
+                        </Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Confirm password"
+                            name="password2"
+                            value={password2}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    {errors.password2?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                    ))}
+                    <Button
+                        className={`${styles.form_button} button`}
+                        type="submit"
+                    >
+                        Sign up
+                    </Button>
+                    <div className={`${styles.link_container} flex-container`}>
+                        <Link className={`${styles.signup_link}`} to="/signin">
+                            I already have an account!
+                        </Link>
+                    </div>
+                    {errors.non_field_errors?.map((message, idx) => (
+                        <Alert key={idx} variant="warning" className="mt-3">
+                            {message}
+                        </Alert>
+                    ))}
+                </Form>
+            </section>
+            <section
+                className={`${styles.back_ground_container} flex-container`}
+            >
+                <div className={`${styles.image_container} flex-container`}>
+                    <img
+                        className={`${styles.back_ground_image} flex-container`}
+                        src={backImage}
+                        alt="Red cinema seats"
+                    />
+                </div>
+            </section>
+        </article>
+    );
+};
 
-            />
-          </Form.Group>
-          {errors.password1?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-          <Form.Group className={`${styles.form_input_container}`} controlId="password2">
-            <Form.Label className={`display-non`}>Confirm password</Form.Label>
-            <Form.Control 
-            type="password" 
-            placeholder="Confirm password"
-            name = "password2"
-            value = {password2}
-            onChange={handleChange}
-
-            />
-          </Form.Group>
-          {errors.password2?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-          <Button className={`${styles.form_button} button`} type="submit">
-            Sign up
-          </Button>
-          <div className={`${styles.link_container} flex-container`}>
-            <Link className={`${styles.signup_link}`} to='/signin'>I already have an account!</Link>
-          </div>
-          {errors.non_field_errors?.map((message, idx) => (
-              <Alert key={idx} variant="warning" className="mt-3">
-                {message}
-              </Alert>
-            ))}
-        </Form>
-        
-
-
-      </section>
-      <section className={`${styles.back_ground_container} flex-container`}>
-        <div className={`${styles.image_container} flex-container`}>
-          <img className={`${styles.back_ground_image} flex-container`} src={backImage} alt="Red cinema seats" />
-
-        </div>
-        
-      </section>
-
-
-    </article>
-  )
-}
-
-export default SignUp
+export default SignUp;

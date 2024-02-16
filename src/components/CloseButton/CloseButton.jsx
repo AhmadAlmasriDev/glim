dfc8edfeb6ac35cf7c1e70743eec751eed72e43c
Ahmad Almasri <ahmad.mohd.almasri@gmail.com>
2024-02-16 14:51:44 +0000

    Fix CloseButton aria-label

--- src/components/CloseButton/CloseButton.jsx
+++ src/components/CloseButton/CloseButton.jsx
@@ -4,6 +4,7 @@ import styles from "./styles/CloseButton.module.css";
 const CloseButton = ({ on_click_function }) => {
     return (
         <button
+            aria-label="close button"
             className={`${styles.close_button}`}
             onClick={() => on_click_function(false)}
         >
